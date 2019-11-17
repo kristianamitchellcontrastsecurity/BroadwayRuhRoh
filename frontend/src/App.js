f1de8a31db8d817d650aaa9e84b79f938262ba0a
Ati Ok <ati.ok@contrastsecurity.com>
2019-11-17 16:10:38 -0500

    Add login component

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,25 +1,12 @@
 import React from 'react';
 import logo from './logo.svg';
-import './App.css';
+import 'semantic-ui-css/semantic.min.css'
+import Login from './components/Authentication/Login';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <h1> Where is Mark Petersen? </h1>
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Login></Login>
     </div>
   );
 }
---
+++ frontend/src/components/Authentication/Login.js
@@ -0,0 +1,46 @@
+import React, { Component } from 'react';
+import { Button, Input, Divider, Segment, List, Grid } from "semantic-ui-react";
+
+export default class Login extends Component {
+  constructor() {
+    super();
+  }
+
+
+  render() {
+    return(
+      <Grid style={{height: '100vh'}} textAlign='center' verticalAlign='middle' columns={3}>
+      <Grid.Row>
+          {/* Column 1 */}
+          <Grid.Column>
+          </Grid.Column>
+          {/* Column 2 */}
+          <Grid.Column>
+              <Segment padded>
+              <h1>Yalp</h1>
+              <Divider horizontal>Sign In</Divider>
+              <List>
+                  <List.Item>
+                      <Input name='email' icon='user' iconPosition='left' placeholder='Enter email...'/>
+                      {/* <Input name='email' icon='user' iconPosition='left' placeholder='Enter email...' onChange={this.handleChange}/> */}
+                  </List.Item>
+                  <List.Item>
+                      <Input name='password' icon='lock' iconPosition='left' placeholder='Enter password...' type="password"/>
+                      {/* <Input name='password' icon='lock' iconPosition='left' placeholder='Enter password...' type="password" onChange={this.handleChange} /> */}
+                  </List.Item>
+                  <Button onClick={this.handleClick}>Login</Button>
+                  {/* <Button onClick={this.handleClick}>Login</Button> */}
+              </List>
+              <Divider horizontal>OR</Divider>
+              {/* <StyledFirebaseAuth uiConfig={uiConfig} firebaseAuth={firebase.auth()}/> */}
+              </Segment>
+          </Grid.Column>
+          {/* Column 3 */}
+          <Grid.Column>
+          </Grid.Column>
+      </Grid.Row>
+  </Grid>
+    );
+  }
+
+};
